a75cbd82b0701b15bff89e231209f7f816fab4f4
jennifer <jenn.chen@live.ca>
2018-05-01 09:33:23 -0700

    Adjust username regex expression

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
-  has_many :plants, dependent: :nullify 
+  has_many :plants, dependent: :nullify
   has_secure_password
+
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :username,
     presence: true,
     uniqueness: true,
-    format: { with: /^[a-z0-9][-a-z0-9]{4,19}$/i, message: 'No special characters, only letters, numbers, and daashes and is between 5-20 characters' }
+    format: { with: VALID_USERNAME_REGEX, message: 'No special characters, only letters, numbers, and daashes and is between 5-20 characters' }
 
   validates :first_name, :last_name, :city, :country, presence: true
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
     presence: true,
     uniqueness: true,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423021139) do
+ActiveRecord::Schema.define(version: 20180501162822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20180423021139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "notes"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +79,5 @@ ActiveRecord::Schema.define(version: 20180423021139) do
   add_foreign_key "images", "plants"
   add_foreign_key "plant_countries", "countries"
   add_foreign_key "plant_countries", "plants"
+  add_foreign_key "plants", "users"
 end
