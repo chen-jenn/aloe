47e3fae91f38b74cd48d5ae9652db3c92a7f9a91
jennifer <jenn.chen@live.ca>
2018-05-06 10:44:16 -0700

    Adjust styling for user show page

--- app/controllers/individual_plants_controller.rb
+++ app/controllers/individual_plants_controller.rb
@@ -1,4 +1,6 @@
 class IndividualPlantsController < ApplicationController
+  before_action :find_individual_plant, only: [:edit, :update, :destroy]
+
   def new
     @individual_plant = IndividualPlant.new
   end
@@ -18,12 +20,22 @@ class IndividualPlantsController < ApplicationController
   end
 
   def update
+    if @individual_plant.update individual_plant_params
+      redirect_to user_path(@individual_plant.user)
+    else
+      flash[:alert] = "Error occurred"
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+  def find_individual_plant
+    @individual_plant = IndividualPlant.find params[:id]
+  end
+
   def individual_plant_params
     params.require(:individual_plant).permit([
       :species_name,
