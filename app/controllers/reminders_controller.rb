1508bb2e14a5a5f95992d1fe6a5c2fd5e96f9340
jennifer <jenn.chen@live.ca>
2018-05-11 12:11:43 -0700

    Allow creation of a reminder

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,14 +1,27 @@
 class RemindersController < ApplicationController
   def create
-    @reminder = Reminder.new
+    i = IndividualPlant.find params[:individual_plant_id]
+    @reminder = Reminder.new({last_reminder: Time.now})
+    @reminder.user = current_user
+    @reminder.individual_plant = i
 
+    if i.water_frequency == 'low'
+      @reminder.frequency = 'once a week'
+    elsif i.water_frequency == 'med'
+      @reminder.frequency = 'twice a week'
+    elsif i.water_frequeny == 'high'
+      @reminder.frequency = 'every other day'
+    end
+
+    if @reminder.save
+      redirect_to user_path(@reminder.user)
+    else
+      flash[:alert] = 'error occurred'
+    end
   end
 
   def destroy
+    # removing a reminder
   end
 
-  private
-  def reminder_params
-    params.require 
-  end
 end
